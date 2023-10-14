213a5d496190b9ea5e3dde40c88032e1c1fb0a73
千橘 雫霞 <i@qianjunakasumi.moe>
2023-10-14 14:23:28 +0800

    feat: add prefetch and sitemap support for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import lit from "@astrojs/lit";
 import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [lit(), tailwind()]
+  integrations: [lit(), tailwind(), prefetch(), sitemap()]
 });
