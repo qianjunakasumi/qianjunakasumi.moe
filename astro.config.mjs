b9bd61720c115700ca99291a9779f5c68cd43890
千橘 雫霞 <i@qianjunakasumi.moe>
2025-02-27 00:43:48 +0000

    feat: upgrade tailwindcss to v4 (#203) close 考虑迁移到 Tailwindcss 4.0 #203

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 import { defineConfig } from 'astro/config';
 import lit from "@astrojs/lit";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://qianjunakasumi.moe',
+
   build: {
     assets: 'assets'
   },
-  integrations: [lit(), tailwind(), sitemap(), mdx()],
+
+  integrations: [lit(), sitemap(), mdx()],
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ja', 'zh'],
@@ -18,5 +21,9 @@ export default defineConfig({
       ja: 'en',
       zh: 'en',
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
