eb2eaa65a6823c14003b7cf56c6f4aa18b6c7aab
千橘 雫霞 <i@qianjunakasumi.moe>
2025-09-03 07:40:54 +0000

    refactor: remove `lit` feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import lit from "@astrojs/lit";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import tailwindcss from '@tailwindcss/vite';
@@ -12,7 +11,7 @@ export default defineConfig({
     assets: 'assets'
   },
 
-  integrations: [lit(), sitemap(), mdx()],
+  integrations: [sitemap(), mdx()],
 
   i18n: {
     defaultLocale: 'en',
